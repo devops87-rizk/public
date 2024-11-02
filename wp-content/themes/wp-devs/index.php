40a4c89447753cc6ce540367ce992ed4bcc37e8e
Rizky Rahman <rizk87devops@gmail.com>
2024-11-02 12:05:59 +0800

    html and paragraph

---
+++ wp-content/themes/wp-devs/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>WP Devs</title>
+</head>
+<body>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur congue ullamcorper viverra. Cras et risus id sapien euismod viverra vitae id arcu. Maecenas tristique purus id urna tincidunt convallis sit amet ac tellus. In pulvinar felis a orci pretium finibus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aenean erat erat, accumsan eget condimentum id, placerat quis justo. Phasellus rutrum vitae arcu sed pharetra. Quisque ultricies augue vitae est pretium sagittis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec odio massa, gravida sit amet mattis quis, laoreet nec quam. Nullam sed vehicula tortor. Vestibulum feugiat nibh quam, eu facilisis leo pretium quis. Nunc et ornare magna. Fusce molestie, lectus id maximus finibus, augue ipsum egestas tortor, ut consectetur neque tortor ut justo. Integer urna tortor, luctus non tristique a, ultrices non urna. Maecenas mattis eros et magna scelerisque porttitor.</p>
+</body>
+</html>
