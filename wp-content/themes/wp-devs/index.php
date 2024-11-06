0a2a2b4bca9e9b575e02306a8c8ec7ca0e323d7f
Rizky Rahman <rizk87devops@gmail.com>
2024-11-06 21:35:26 +0800

    header and footer

--- wp-content/themes/wp-devs/index.php
+++ wp-content/themes/wp-devs/index.php
@@ -1,28 +1,4 @@
-<!DOCTYPE html>
-<html <?php language_attributes(); ?>>
-<head>
-    <meta charset="<?php bloginfo('charset'); ?>">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-</head>
-<body <?php body_class(); ?>>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur congue ullamcorper viverra. Cras et risus id sapien euismod viverra vitae id arcu. Maecenas tristique purus id urna tincidunt convallis sit amet ac tellus. In pulvinar felis a orci pretium finibus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aenean erat erat, accumsan eget condimentum id, placerat quis justo. Phasellus rutrum vitae arcu sed pharetra. Quisque ultricies augue vitae est pretium sagittis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec odio massa, gravida sit amet mattis quis, laoreet nec quam. Nullam sed vehicula tortor. Vestibulum feugiat nibh quam, eu facilisis leo pretium quis. Nunc et ornare magna. Fusce molestie, lectus id maximus finibus, augue ipsum egestas tortor, ut consectetur neque tortor ut justo. Integer urna tortor, luctus non tristique a, ultrices non urna. Maecenas mattis eros et magna scelerisque porttitor.</p>
-    <div id="page" class="site">
-        <header>
-            <section class="top-bar">
-                <div class="logo">
-                    logo
-                </div>
-                <div class="searchbox">
-                    searchbox          
-                </div> 
-            </section>
-            <section class="menu-area">
-                <nav class="main-menu">
-                    menu
-                </nav>
-            </section>
-        </header>
-        <!-- -->
+<?php get_header(); ?>
         <div id="content" class="site-content">
             <div id="primary" class="content-area">
                 <main id="main" class="site-main">
@@ -38,10 +14,4 @@
                 </main>
             </div>
         </div>
-        <!-- -->
-        <footer class="site-footer">
-            footer
-        </footer>
-    </div>
-</body>
-</html>
+<?php get_footer(); ?> 
