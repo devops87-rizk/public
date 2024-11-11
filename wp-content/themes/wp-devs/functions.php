41eef7c00ed8453368aadbb69fb9e90fc7983ad0
Rizky Rahman <rizk87devops@gmail.com>
2024-11-11 10:46:12 +0800

    wp-head

--- wp-content/themes/wp-devs/functions.php
+++ wp-content/themes/wp-devs/functions.php
@@ -1,6 +1,6 @@
 <?php
 
-    function wpdevs_load_scripts() {
-        wp_enqueue_style('wpdevs-style', get_stylesheet_uri(), array(), '1.0', 'all');
+    function fictionalportofolio_load_scripts() {
+        wp_enqueue_style('fictionalportofolio-style', get_stylesheet_uri(), array(), filemtime (get_stylesheet_directory(). '/style.css'), 'all');
     }
-    add_action ('wp_enqueue_scripts', 'wpdevs_load_scripts');
+    add_action ('wp_enqueue_scripts', 'fictionalportofolio_load_scripts');
