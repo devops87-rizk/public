2a51cb4dd45a14d06d044e36357d5a895c295273
Rizky Rahman <rizk87devops@gmail.com>
2024-11-12 14:55:07 +0800

    add enqueue js

--- wp-content/themes/wp-devs/functions.php
+++ wp-content/themes/wp-devs/functions.php
@@ -3,5 +3,6 @@
     function fictionalportofolio_load_scripts() {
         wp_enqueue_style('fictionalportofolio-style', get_stylesheet_uri(), array(), filemtime (get_stylesheet_directory(). '/style.css'), 'all');
         wp_enqueue_style('google-fonts', 'https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap" rel="stylesheet', array(), null);
+        wp_enqueue_script('dropdown', get_template_directory_uri() .'/js/dropdown.js', array(), '1.0', true);
     }
     add_action ('wp_enqueue_scripts', 'fictionalportofolio_load_scripts');
