1123604a0eb9a4520d99732f50a2a7be225820e1
Rizky Rahman <rizk87devops@gmail.com>
2024-11-12 14:51:36 +0800

    add google font

--- wp-content/themes/wp-devs/functions.php
+++ wp-content/themes/wp-devs/functions.php
@@ -2,5 +2,6 @@
 
     function fictionalportofolio_load_scripts() {
         wp_enqueue_style('fictionalportofolio-style', get_stylesheet_uri(), array(), filemtime (get_stylesheet_directory(). '/style.css'), 'all');
+        wp_enqueue_style('google-fonts', 'https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap" rel="stylesheet', array(), null);
     }
     add_action ('wp_enqueue_scripts', 'fictionalportofolio_load_scripts');
